47719672a683aaa1a4028acd5a0f106ea1a98db9
Todd Tate <toddtatedev@gmail.com>
2019-09-25 13:22:27 -0600

    Added SendGrid API Key

--- mail.php
+++ mail.php
@@ -31,7 +31,7 @@ $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+$sendgrid = new \SendGrid(getenv('0VjPeUWcxP0CayoRqpkMvZny1gEM_hvLgF1AFfZUi68'));
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
